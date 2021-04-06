77007f375f8f64fe654bd1a84935cf2352866a7a
jiangzy <jiangzy1998@gmail.com>
2021-04-06 10:26:19 +0800

    设置通配符路由，设置重定向

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
-    path:"first-component",
+    path:'first-component',
     component:FirstComponent
   },
   {
-    path:"second-component",
+    path:'second-component',
     component:SecondComponent
+  },
+  {
+    path:'',
+    redirectTo:'/first-component',
+    //pathMatch=‘full’ 路由器应该只有在*完整的URL_等于''时，才进行重定向
+    pathMatch:'full'
+  },
+  {
+    //设置一个通配符路由
+    path:'**',
+    component:PageNotFoundComponent
   }
 ];
 
